6325a833d85eb0117696f1bc10b85bcad0a8f323
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-02-28 13:46:48 +0100

    removed deprecated should from specs

--- spec/features/force_login_spec.rb
+++ spec/features/force_login_spec.rb
@@ -1,9 +1,9 @@
 describe "force the signup process", :type => :feature do
 
-  it "should redirect to login" do
+  it "redirects to login" do
     visit '/reports'
     within 'h2' do
-      page.should have_content 'Log in'
+      expect(page).to have_content 'Log in'
     end
   end
 end
