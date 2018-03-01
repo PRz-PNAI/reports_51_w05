4eb8f5b8cbabf3418fcb3b89b3176a0dcb817cea
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-01 16:49:38 +0100

    report factory with traits for grade and comment

--- spec/factories/report.rb
+++ spec/factories/report.rb
@@ -4,5 +4,12 @@ FactoryBot.define do
     last_name "Chrobry"
     email "BoleslawChrobry@piastowie.gov"
     content "Testowa treść sprawozdania. " * 20
+
+    trait :grade do
+      grade "3.5"
+    end
+    trait :comment do
+      comment "Niezłe sprawozdanie. Tylko, że nie na temat..."
+    end
   end
 end
