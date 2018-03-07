1683d1f2f0f80508023e898e1783c99c0ecbc573
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-07 14:04:41 +0100

    Added more spec cases for email format

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -8,8 +8,8 @@ describe Report do
   it { is_expected.to validate_length_of(:first_name).is_at_least(2).is_at_most(40) }
   it { is_expected.to validate_length_of(:last_name).is_at_least(3).is_at_most(40) }
 
-  it { is_expected.not_to allow_values("asd", "asd.pl", "@asd.pl", "asd@asd").for(:email) }
-  it { is_expected.to allow_values("asd@asd.pl", "asd@asd.com", "dsa@ad.com.pl").for(:email) }
+  it { is_expected.not_to allow_values("asd", "asd.pl", "@asd.pl", "asd@asd","asd@fgh@sfd.pl", "dsa@.com.pl").for(:email) }
+  it { is_expected.to allow_values("asd@asd.pl", "asd@asd.com", "dsa@ad.com.pl", "asd.ewq@qwe.gov").for(:email) }
 
   it { is_expected.not_to validate_presence_of :grade }
   it { is_expected.not_to validate_presence_of :comment }
