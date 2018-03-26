8bd41eb72731ab81711a035e32ad6c24f83f89a2
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-26 18:02:07 +0200

    Implemented registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,21 @@
 class RegistrationsController < ApplicationController
   def new
+    @registration = RegistrationForm.new
   end
 
   def create
+    @registration = RegistrationForm.new(reg_params)
+    if @registration.valid?
+      logger.warn "Registration is valid!"
+      # TODO: put creating user and topic here!
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def reg_params
+    params.require(:registration).permit(:first_name, :last_name, :title, :topic_name, :email, :password, :password_confirmation, :title, :topic)
   end
 end
