2612d84b6aa66dd2a3d12820fc66a45ccb648ac1
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-26 17:22:50 +0200

    Generated and loaded migration with user details:
    rails generate migration add_user_details_to_users first_name:string last_name:string title:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326151806) do
+ActiveRecord::Schema.define(version: 20180326152207) do
 
   create_table "reports", force: :cascade do |t|
     t.string "first_name"
@@ -43,6 +43,9 @@ ActiveRecord::Schema.define(version: 20180326151806) do
     t.string "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "title"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
