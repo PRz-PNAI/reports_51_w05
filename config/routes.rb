22d0c66294c3010f1b552cc1fc4b3606fa54d82a
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-26 16:07:17 +0200

    Generated controller for Contact form with: rails generate controller Contact new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'contact/new'
+
+  get 'contact/create'
+
   devise_for :users
   resources :reports
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
