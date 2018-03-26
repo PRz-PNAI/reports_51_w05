86c3ea79d7e9ddb240607e04cf80e9de020e1946
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-26 17:20:14 +0200

    Generated migration for topics foreign key and suplemented fk definition:
    rails generate migration add_user_id_to_topics user_id:numeric

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180326151723) do
+ActiveRecord::Schema.define(version: 20180326151806) do
 
   create_table "reports", force: :cascade do |t|
     t.string "first_name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180326151723) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
