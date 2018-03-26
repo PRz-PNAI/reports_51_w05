37fa00a1b8990e03a564cf1a0f3cfe10f4d523d3
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-26 19:18:45 +0200

    Form objects respects also submodel validations

--- app/forms/registration_form.rb
+++ app/forms/registration_form.rb
@@ -10,4 +10,27 @@ class RegistrationForm
     ActiveModel::Name.new(self, nil, "Registration")
   end
 
+  def valid?
+    validate
+    [ user_model, topic_model ].each do |o|
+      o.validate
+      o.errors.each { |field, error| errors.add field, error }
+    end
+    errors.empty?
+  end
+
+  private
+
+  def user_model
+    @user ||= User.new  title: title,
+                        first_name: first_name,
+                        last_name: last_name,
+                        email: email,
+                        password: password
+  end
+
+  def topic_model
+    @topic_model ||= Topic.new name: topic, user: user_model
+  end
+
 end
