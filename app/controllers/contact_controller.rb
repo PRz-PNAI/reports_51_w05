e2e7298f78c3db1ee77fbbb8d253e02d86b36e03
wrzasa <wrzasa@rzeszow.oaza.pl>
2018-03-26 16:18:32 +0200

    Implemented controller actions

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,7 +1,21 @@
 class ContactController < ApplicationController
   def new
+    @contact = ContactForm.new
   end
 
   def create
+    @contact = ContactForm.new(contact_params[:contact])
+    if @contact.valid?
+      # send contact email
+      logger.warn "Sending email from: #{@contact.email}"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :email, :message)
   end
 end
